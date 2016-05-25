f6aa10cb4f280372e8d6a64e8eae670fd67467d9
prajwal967 <prajwal967@gmail.com>
2016-05-26 01:02:15 +0530

    Base Models
    Linear Regression, Logistic Regression (L1 & L2), Decision Trees, Random Forests, Multi Layer Perceptron have been implemented.
    Cross Validation : Stratified K Fold

--- Base Models.py
+++ Base Models.py
@@ -7,6 +7,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.cross_validation import KFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import StratifiedKFold
+from keras.layers import Dense, Activation
+from keras.models import Sequential
 
 encode = preprocessing.LabelEncoder()
 Data = pd.read_csv('/home/prajwal/Desktop/bank-additional/bank-additional-full.csv',delimiter=';',header=0)
@@ -33,11 +35,14 @@ mean_DT=list()
 avg_DT=0
 mean_RF=list()
 avg_RF=0
+mean_MLP=list()
+avg_MLP=0
 
 variance_L2=list()
 variance_L1=list()
 variance_DT=list()
 variance_RF=list()
+variance_MLP=list()
 
 kf = StratifiedKFold(Data['y'], n_folds=5,shuffle=True)
 
@@ -48,6 +53,16 @@ for train_index, cross_val_index in kf:
     cross_val_Y = cross_val['y']
     cross_val_X = cross_val.drop(['y'], axis=1)
     predict = list()
+       
+    #Multi Layer Perceptron
+    model = Sequential()
+    model.add(Dense(output_dim=64, input_dim=20, init='uniform', activation='sigmoid'))
+    model.add(Dense(output_dim=1, input_dim=64,activation='sigmoid'))
+    model.compile(optimizer='rmsprop',loss='mean_squared_error',metrics=['accuracy'])
+    model.fit(train_X.as_matrix(), train_Y.as_matrix(), nb_epoch=5, batch_size=32)
+    eval=model.test_on_batch(cross_val_X.as_matrix(), cross_val_Y.as_matrix(), sample_weight=None)
+    mean_MLP.append(eval[0])
+    variance_MLP.append(eval[1])
 
     # Decision Tree
     model = DecisionTreeClassifier()
@@ -96,9 +111,11 @@ avg_L2=np.mean(mean_L2)
 avg_L1=np.mean(mean_L1)
 avg_DT=np.mean(mean_DT)
 avg_RF=np.mean(mean_RF)
+avg_MLP=np.mean(mean_MLP)
 
 print (' Mean Error (Linear Regression)\n',avg_LinearRegression)
 print (' Mean Error (Logistic Regression - L2)\n',avg_L2)
 print (' Mean Error (Logistic Regression - L1)\n',avg_L1)
 print (' Mean Error (Decision Tree)\n',avg_DT)
 print (' Mean Error (Random Forest)\n',avg_RF)
+print (' Mean Error (Multi Layer Perceptron)\n',avg_MLP)
