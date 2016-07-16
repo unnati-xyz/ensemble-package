34fe895cd341b389ae3d95c91b726fcc7259a364
prajwal967 <prajwal967@gmail.com>
2016-07-16 15:53:17 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ensemble',
+  packages = ['ensemble'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A package for ensembling of machine learning models',
+  author = 'Prajwal Kailas',
+  author_email = 'prajwal967@gmail.com',
+  url = 'https://github.com/unnati-xyz/ensemble-package', # use the URL to the github repo
+  download_url = 'https://github.com/unnati-xyz/ensemble-package/tarball/0.1', # I'll explain this in a second
+  keywords = ['Base Models', 'Ensemble Models', 'Training', 'Testing'], # arbitrary keywords
+  classifiers = [],
+)
